23f2dc91445aad581b36277aad8c1503861d6c01
Nick Rothera <nickrothera@gmail.com>
2020-01-19 21:36:09 +0000

    first commit

---
+++ APITesting/APITesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("APITesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("APITesting")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("899fba98-879b-498d-ad73-c6479a4a026c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ APITesting/APITesting/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+
+namespace APITesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var client = new RestClient("http://localhost:3000/");
+
+            var request = new RestRequest("post/{postid}", Method.GET);
+
+            request.AddUrlSegment("postid", 1);
+
+            var content = client.Execute(request).Content;
+
+
+        }
+    }
+}
